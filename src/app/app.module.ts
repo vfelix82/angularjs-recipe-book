09838209bb9ff89ad9385004c60ff84a96cc23d8
Victor Felix <vfelix82@gmail.com>
2018-03-30 10:16:38 -0700

    add base recipe-project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { RecipeDetailsComponent } from './recipe/recipe-details/recipe-details.component';
+import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeFormComponent } from './recipe/recipe-start/recipe-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeComponent,
+    RecipeDetailsComponent,
+    RecipeEditComponent,
+    RecipeListComponent,
+    RecipeFormComponent
   ],
   imports: [
     BrowserModule
